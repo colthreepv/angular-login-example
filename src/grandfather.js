e8a4c3ae675e703fe16f3638e859f28747f66054
mrgamer <mister.gamer@gmail.com>
2013-08-31 13:28:15 +0200

    grandfather does his job now.

--- src/grandfather.js
+++ src/grandfather.js
@@ -6,16 +6,38 @@ angular.module('angular-login.grandfather', ['ui.router', 'templates-app'])
       template: '<ui-view></ui-view>',
       resolve: {
         'login': function (loginService, $http, $q, $state) {
-          var loginDefer = $q.defer(), loginPromise;
+          var checkUser = $q.defer(),
+              loginPromise,
+              finalPromise,
+              pendingState = loginService.pendingStateChange;
 
-          if (loginService.pendingStateChange) {
+          if (pendingState) {
             loginPromise = $http.get('/user');
+            // When the $http is done, we register the http result into setPermissions, `data` parameter goes into loginService.setPermissions
             loginPromise.success(loginService.setPermissions);
-            return loginPromise;
+            loginPromise.error(function () {
+              checkUser.reject('tokenexpired');
+            });
+
+            /**
+             * Define another check after the $http is done, this will *Actually* check if current user can access the requested $state
+             */
+            finalPromise = $q.all([loginPromise, checkUser.promise]);
+
+            loginPromise.then(function (result) {
+              // duplicated logic from loginService $stateChangeStart, slightly different, now we *MUST* have the accessLevel informations.
+              if (pendingState.to.accessLevel === undefined || pendingState.to.accessLevel.bitMask & loginService.accessLevel.bitMask) {
+                checkUser.resolve();
+              } else {
+                checkUser.reject('unauthorized');
+              }
+            });
+
+            return finalPromise;
           } else {
-            loginDefer.resolve();
+            checkUser.resolve();
           }
-          return loginDefer.promise;
+          return checkUser.promise;
         }
       }
     });

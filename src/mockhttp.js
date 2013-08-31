8eaefbf90d3415b01c5d6eff1b0dad8e627f76fc
mrgamer <mister.gamer@gmail.com>
2013-08-31 13:27:21 +0200

    userRoles fixed

--- src/mockhttp.js
+++ src/mockhttp.js
@@ -23,8 +23,8 @@ angular.module('angular-login.mock', ['ngMockE2E'])
 
   if (userStorage === null) {
     userStorage = {
-      'johnm': { name: 'John', password: 'hello', accessLevel: accessLevels.user, tokens: [] },
-      'sandrab': { name: 'Sandra', password: 'world', accessLevel: accessLevels.admin, tokens: [] }
+      'johnm': { name: 'John', password: 'hello', accessLevel: userRoles.user, tokens: [] },
+      'sandrab': { name: 'Sandra', password: 'world', accessLevel: userRoles.admin, tokens: [] }
     };
     localStorage.setItem('userStorage', JSON.stringify(userStorage));
   }

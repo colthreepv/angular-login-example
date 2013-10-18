5c9baedb8af53988f8d049f5d83cbab70753274f
mrgamer <mister.gamer@gmail.com>
2013-10-18 18:47:12 +0200

    Code to use timeout on $http taken from brian's ford http://plnkr.co/edit/P8wKns51GVqw5mwS5l5R?p=preview
    WIP

--- src/register/register.js
+++ src/register/register.js
@@ -8,7 +8,28 @@ angular.module('angular-login.register', ['angular-login.grandfather'])
       accessLevel: accessLevels.anon
     });
 })
-.controller('RegisterController', function ($scope, $http) {
+.factory('httpQuery', function ($http, $httpBackend, $rootScope, $q) {
+  return function (url) {
+    var cancelQuery = null;
+    return function runQuery(query) {
+      // if we were running a query before,
+      // cancel it so it doesn't invoke its success callback
+      if (cancelQuery) {
+        cancelQuery.resolve();
+      }
+      cancelQuery = $q.defer();
+      return $http
+        .get(url, {
+          timeout: cancelQuery.promise
+        })
+        .success(function (data, status) {
+          console.log('response');
+          cancelQuery = null;
+        });
+    };
+  };
+})
+.controller('RegisterController', function ($scope, httpQuery) {
   var first = true,
       cancelHttp = null;
 
@@ -19,12 +40,25 @@ angular.module('angular-login.register', ['angular-login.grandfather'])
     email: undefined
   };
 
+  var someQuery = httpQuery('style.css');
+
+  /**
+   * continuos form remote checking
+   */
   $scope.$watch('registerObj', function (newValue, oldValue, scope) {
     if (first) {
       first = !first;
       return;
     }
+
+    var httpPromise = someQuery();
+    $scope.status = 'loading...';
+    httpPromise.success(function (data) {
+      $scope.result = data;
+      $scope.status = 'done!';
+    });
   }, true);
+
   $scope.submit = function () {
     // $http here.
   };

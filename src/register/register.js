b4d44bb80ef7e26692cd02374d78e1ff7c921731
mrgamer <mister.gamer@gmail.com>
2013-11-21 16:09:51 +0100

    refactor(registration): perfect user feedback, AFAIK!

--- src/register/register.js
+++ src/register/register.js
@@ -8,8 +8,9 @@ angular.module('angular-login.register', ['angular-login.grandfather'])
       accessLevel: accessLevels.anon
     });
 })
-.controller('RegisterController', function ($scope, $http) {
-  var xhr = false;
+.controller('RegisterController', function ($scope, $http, $timeout, $state) {
+  $scope.xhr = false;
+  $scope.redirect = false;
 
   $scope.registerObj = {
     username: undefined,
@@ -19,15 +20,24 @@ angular.module('angular-login.register', ['angular-login.grandfather'])
   };
 
   $scope.submit = function (formInstance) {
+    // xhr is departing
+    $scope.xhr = true;
     $http.post('/user', $scope.registerObj)
     .success(function (data, status, headers, config) {
       console.info('post success - ', data);
+      $scope.xhr = false;
+      $scope.redirect = true;
+      $timeout(function () {
+        $state.go('app.home');
+      }, 2000);
     })
     .error(function (data, status, headers, config) {
       data.errors.forEach(function (error, index, array) {
         formInstance[error.field].$error[error.name] = true;
       });
+      formInstance.$setPristine();
       console.info('post error - ', data);
+      $scope.xhr = false;
     });
   };
 });
